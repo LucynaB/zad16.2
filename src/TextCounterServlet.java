10b15e20e201f7e8c6a75c1aa8dc86c4b0b5edb6
lblaszczyk <lusia22b@wp.pl>
2018-08-27 10:40:03 +0200

    zad 16.2 popr

--- src/TextCounterServlet.java
+++ src/TextCounterServlet.java
@@ -9,17 +9,21 @@ import java.io.PrintWriter;
 @WebServlet("/textCounter")
 public class TextCounterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");//żeby pobrać tekst z polskimi znakami
         String text = request.getParameter("ta");
+
 
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html");
 
         PrintWriter writer = response.getWriter();
-        writer.println(text);
-        writer.println("Ilość słów: " + TextCounter.countWords(text));
-        writer.println("Ilość znaków: " + TextCounter.countChars(text));
-        writer.println("Ilość znaków (bez spacji): " + TextCounter.countCharsWithoutSpaces(text));
-        writer.println("Palindrom: " + TextCounter.checkPalindrome(text));
+        writer.println("<p><i>"+text+"</i></p>");
+        writer.println("<h2>Ilość słów: " + TextCounter.countWords(text)+"</h2>");
+        writer.println("<h2>Ilość znaków: " + TextCounter.countChars(text)+"</h2>");
+        writer.println("<h2>Ilość znaków (bez spacji): " + TextCounter.countCharsWithoutSpaces(text)+"</h2>");
+        writer.println("<h2>Palindrom: " + TextCounter.checkPalindrome(text)+"</h2>");
+
+
 
     }
 
